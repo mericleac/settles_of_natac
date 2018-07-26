60875a121394f95e9eec51c8fd1126eb9ebbcffe
Jeremy Veto <jveto91@gmail.comgit config --global user.name jvetogit commit -m testing_first_commitlssotpstopgithelp--global user.email jveto91@gmail.com>
2018-07-26 16:05:43 -0500

    trading done

--- apps/trading/urls.py
+++ apps/trading/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^/(?P<id>\d+)$', views.index),
-    url(r'^/create/(?P<id>\d+)$', views.create),
-    url(r'^/trade$', views.trade),
-    url(r'^/delete$', views.delete),
+    url(r'(?P<id>\d+)$', views.index),
+    url(r'trade$', views.trade),
+    url(r'delete$', views.delete),
 ]
