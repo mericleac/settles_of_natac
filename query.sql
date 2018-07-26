1d679ff86097b752559af9232928d83833de73a1
Jeremy Veto <jveto91@gmail.comgit config --global user.name jvetogit commit -m testing_first_commitlssotpstopgithelp--global user.email jveto91@gmail.com>
2018-07-26 13:17:49 -0500

    trading work

--- query.sql
+++
@@ -1,8 +0,0 @@
-select * from main_game_settlement
-LEFT JOIN main_game_settlement_adjacent_fields on main_game_settlement.id = main_game_settlement_adjacent_fields.settlement_id
-LEFT JOIN main_game_field on main_game_settlement_adjacent_fields.field_id = main_game_field.id
-WHERE main_game_settlement.id = 22;
-
-select * from main_game_road
-LEFT JOIN main_game_road_adjacent_settlements on main_game_road.id = main_game_road_adjacent_settlements.road_id
-LEFT JOIN main_game_settlement on main_game_road_adjacent_settlements.settlement_id = main_game_settlement.id;
